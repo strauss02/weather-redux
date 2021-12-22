9dfd72f9c80f644dd467357728a09ebd188faad5
Ido Strauss <strauss02@hotmail.co.il>
2021-12-23 00:58:33 +0200

    add css

--- weather-redux/src/App.js
+++ weather-redux/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
+import { changeCity } from './features/weather/weatherSlice'
 import './App.css'
 
 function App() {
   const currentCity = useSelector((state) => state.weather.currentCity)
+  const weather = useSelector((state) => state.weather)
   const dispatch = useDispatch()
+
+  const [cityInput, setCityInput] = useState('')
 
   return (
     <div className="App">
-      <input placeholder="enter city name" />
-      <button>get weather</button>
-      <div className="forecast">
-        <div></div>
+      <div className="header">
+        <input
+          value={cityInput}
+          onChange={(e) => setCityInput(e.target.value)}
+          placeholder="enter city name"
+        />
+        <button onClick={() => dispatch(changeCity(cityInput))}>
+          get weather
+        </button>
       </div>
+      {weather ? (
+        <div className="forecast">
+          <h3 className="temperature"> {weather.main.temp}</h3>
+          <h3 className="description"> {weather.weather[0].description} </h3>
+        </div>
+      ) : null}
     </div>
   )
 }
