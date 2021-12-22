76e8baf15ff0697cd063a56f2e20799f29ba5880
Ido Strauss <strauss02@hotmail.co.il>
2021-12-22 15:04:40 +0200

    clean extra nodemodules

--- weather-redux/src/app/store.js
+++ weather-redux/src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+import weatherReducer from '../features/weather/weatherSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    weather: weatherReducer,
   },
-});
+})
